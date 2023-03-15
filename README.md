2b80add401734b546d8f3de5f0200e0bb5018737
dmantilla17 <derekaraujo11@gmail.com>
2023-03-15 17:52:42 -0400

    created a function to show questions

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 ## Title
+ReadMe Generator
 
-##
+## Motivation
+Created a readMe generator using node js to convinently create readMe for upcoming projects 
+
+## Solutions 
+
+
+## Example of the website 
+Upcoming
+
+## What i learned 
--- index.js
+++ index.js
@@ -1,73 +1,41 @@
 //DEPENDENCIES
-const { prompt } = require("inquirer");
-const { writeFile} = require("fs");
+const inquirer = require("inquirer");
+const { writeFile } = require("fs");
 //DATA
 const questions = [
   {
     type: "input",
-    question: "What is the name of your project?",
+    message: "What is the name of your project?",
     name: "name",
   },
   {
     type: "input",
-    question: "What is the motivation of your project?",
+    message: "What is the motivation of your project?",
     name: "motivation",
   },
   {
     type: "input",
-    question: "What probelms does it solve?",
+    message: "What probelms does it solve?",
     name: "solutions",
   },
   {
     type: "input",
-    question: "Insert example of your project",
+    message: "Insert example of your project",
     name: "example",
   },
   {
     type: "input",
-    question: "What did you learn?",
+    message: "What did you learn?",
     name: "learned",
   },
 ];
 
 //FUNCTIONS
-const generateReadMe= ({ name,motivation,solutions,example,learned }) => {
-  function generateMarkDown(answers){
-    const markDownText= writeToFile;
-  }
-    const template= 'README.md';
-    return template; 
-  
-}
+showQuestions();
 
-function writeToFile(READMEmd); {
-    const fileName= (READMEmd);
-    return fileName;
-};
-
-const init = () => {
-  //prompt the user to answer questions for the question bank
-  prompt(questions).then((answers) => {
-    console.log(questions);
-    //generate readMe for answers
-    
-   const readMe= generateReadMe(answers);
+function showQuestions() {
+  console.log("show questions");
+  inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
   });
-  init();
-};
-
-
-//USER INTERACTIONS
-inquirer.prompt(questions)
-.then((answers) => {;
-const markDown= generateMarkDown(answers);
-});
-fetch((error)=> {
-  if(error.error){
-  } else {
-    console.log("Error, Try again");
-  }
-});
-
-
-//INITIALZATIONS
+}
